80e38776efe6faf1567315c0e001c70540bee33b
Marko Tintor <tintor@gmail.com>
2017-04-22 16:17:30 -0700

    original:2 solved non-optimally

--- src/tintor/sokoban/NonOptimalSolverTest.java
+++ src/tintor/sokoban/NonOptimalSolverTest.java
@@ -7,13 +7,22 @@ import tintor.sokoban.Solver.Context;
 
 public class NonOptimalSolverTest {
 	@Test(timeout = 1500)
-	public void solve() {
-		Level level = new Level("test:1");
+	public void solve_test_1() {
+		solve("test:1", 550);
+	}
+
+	@Test(timeout = 1500)
+	public void solve_original_2() {
+		solve("original:2", 492);
+	}
+
+	private void solve(String filename, int expected) {
+		Level level = new Level(filename);
 		Context context = new Context();
 		context.optimal_macro_moves = false;
 		context.greedy_score = true;
 		State[] solution = Solver.solve_Astar(level, level.start, new Heuristic(level), new Deadlock(level), context);
 		Assert.assertTrue(solution != null);
-		Assert.assertEquals(550, solution[solution.length - 1].dist());
+		Assert.assertEquals(expected, solution[solution.length - 1].dist());
 	}
 }
