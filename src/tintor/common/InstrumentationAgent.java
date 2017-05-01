af64c7e7d58ae304aeaef454bd7905d3db5dc6e5
Marko Tintor <tintor@gmail.com>
2017-04-30 19:39:56 -0700

    Update InstrumentationAgent

--- src/tintor/common/InstrumentationAgent.java
+++ src/tintor/common/InstrumentationAgent.java
@@ -11,83 +11,60 @@ import java.util.Stack;
 public class InstrumentationAgent {
 	private static Instrumentation instrumentation;
 
-	/** Initializes agent */
 	public static void premain(String agentArgs, Instrumentation instrumentation) {
 		InstrumentationAgent.instrumentation = instrumentation;
 	}
 
-	/** Returns object size. */
 	public static long sizeOf(Object obj) {
-		if (instrumentation == null) {
-			throw new IllegalStateException("Instrumentation environment not initialised.");
-		}
-		if (isSharedFlyweight(obj)) {
-			return 0;
-		}
-		return instrumentation.getObjectSize(obj);
+		return isSharedFlyweight(obj) ? 0 : instrumentation.getObjectSize(obj);
 	}
 
-	/**
-	 * Returns deep size of object, recursively iterating over
-	 * its fields and super classes.
-	 */
 	public static long deepSizeOf(Object obj) {
 		Map<Object, Object> visited = new IdentityHashMap<Object, Object>();
 		Stack<Object> stack = new Stack<Object>();
 		stack.push(obj);
-
 		long result = 0;
-		do {
+		while (!stack.isEmpty())
 			result += internalSizeOf(stack.pop(), stack, visited);
-		} while (!stack.isEmpty());
 		return result;
 	}
 
-	/**
-	 * Returns true if this is a well-known shared flyweight.
-	 * For example, interned Strings, Booleans and Number objects
-	 */
 	private static boolean isSharedFlyweight(Object obj) {
-		// optimization - all of our flyweights are Comparable
+		// optimization - all of our fly weights are Comparable
 		if (obj instanceof Comparable) {
-			if (obj instanceof Enum) {
+			if (obj instanceof Enum)
 				return true;
-			} else if (obj instanceof String) {
+			if (obj instanceof String)
 				return (obj == ((String) obj).intern());
-			} else if (obj instanceof Boolean) {
+			if (obj instanceof Boolean)
 				return (obj == Boolean.TRUE || obj == Boolean.FALSE);
-			} else if (obj instanceof Integer) {
+			if (obj instanceof Integer)
 				return (obj == Integer.valueOf((Integer) obj));
-			} else if (obj instanceof Short) {
+			if (obj instanceof Short)
 				return (obj == Short.valueOf((Short) obj));
-			} else if (obj instanceof Byte) {
+			if (obj instanceof Byte)
 				return (obj == Byte.valueOf((Byte) obj));
-			} else if (obj instanceof Long) {
+			if (obj instanceof Long)
 				return (obj == Long.valueOf((Long) obj));
-			} else if (obj instanceof Character) {
+			if (obj instanceof Character)
 				return (obj == Character.valueOf((Character) obj));
-			}
 		}
 		return false;
 	}
 
-	private static boolean skipObject(Object obj, Map<Object, Object> visited) {
-		return obj == null || visited.containsKey(obj) || isSharedFlyweight(obj);
-	}
-
 	private static long internalSizeOf(Object obj, Stack<Object> stack, Map<Object, Object> visited) {
-		if (skipObject(obj, visited)) {
+		if (obj == null || visited.containsKey(obj) || isSharedFlyweight(obj))
 			return 0;
-		}
 
 		Class<?> clazz = obj.getClass();
 		if (clazz.isArray()) {
-			addArrayElementsToStack(clazz, obj, stack);
+			if (!clazz.getComponentType().isPrimitive())
+				for (int i = 0; i < Array.getLength(obj); i++)
+					stack.add(Array.get(obj, i));
 		} else {
 			// add all non-primitive fields to the stack
 			while (clazz != null) {
-				Field[] fields = clazz.getDeclaredFields();
-				for (Field field : fields) {
+				for (Field field : clazz.getDeclaredFields())
 					if (!Modifier.isStatic(field.getModifiers()) && !field.getType().isPrimitive()) {
 						field.setAccessible(true);
 						try {
@@ -96,20 +73,10 @@ public class InstrumentationAgent {
 							throw new RuntimeException(ex);
 						}
 					}
-				}
 				clazz = clazz.getSuperclass();
 			}
 		}
 		visited.put(obj, null);
 		return sizeOf(obj);
-	}
-
-	private static void addArrayElementsToStack(Class<?> clazz, Object obj, Stack<Object> stack) {
-		if (!clazz.getComponentType().isPrimitive()) {
-			int length = Array.getLength(obj);
-			for (int i = 0; i < length; i++) {
-				stack.add(Array.get(obj, i));
-			}
-		}
 	}
 }
