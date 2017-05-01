21adb16f68846d0d571b8b07f531d2877a0ff6cf
Marko Tintor <tintor@gmail.com>
2017-04-30 19:59:36 -0700

    CompactArray fixes

--- src/tintor/common/CompactArray.java
+++ src/tintor/common/CompactArray.java
@@ -13,12 +13,34 @@ public final class CompactArray<T> {
 		return values.length;
 	}
 
-	public int first() {
+	public int begin() {
 		return Bits.count_leading_zeroes(bits);
 	}
 
 	public int next(int index) {
-		return Bits.count_leading_zeroes_after(bits, index);
+		int a = nextFast(index);
+		assert a == nextSlow(index);
+		return a;
+	}
+
+	int nextFast(int index) {
+		int i = index / 64;
+		if (index % 64 < 63) {
+			long current = bits[i] & ~((1l << (index + 1)) - 1);
+			if (current != 0)
+				return i * 64 + Long.numberOfTrailingZeros(current);
+		}
+		for (i += 1; i < bits.length; i++)
+			if (bits[i] != 0)
+				return i * 64 + Long.numberOfTrailingZeros(bits[i]);
+		return bits.length * 64;
+	}
+
+	int nextSlow(int index) {
+		for (index += 1; index < bits.length * 64; index++)
+			if (Bits.test(bits, index))
+				break;
+		return index;
 	}
 
 	public int end() {
@@ -26,12 +48,13 @@ public final class CompactArray<T> {
 	}
 
 	public boolean contains(int index) {
-		return Bits.test(bits, index);
+		return index < bits.length * 64 && Bits.test(bits, index);
 	}
 
 	public T get(int index) {
-		if (!Bits.test(bits, index))
+		if (index >= bits.length * 64 || !Bits.test(bits, index))
 			return null;
+		assert Bits.count(bits) == values.length;
 		return (T) values[Bits.count_before(bits, index)];
 	}
 
@@ -41,19 +64,20 @@ public final class CompactArray<T> {
 			return;
 		}
 		if (index >= bits.length * 64)
-			bits = Arrays.copyOf(bits, Util.roundUpPowerOf2((index + 63) / 64));
+			bits = Arrays.copyOf(bits, Util.roundUpPowerOf2(index / 64 + 1));
 		int q = Bits.count_before(bits, index);
 		if (!Bits.test(bits, index)) {
 			Bits.set(bits, index);
 			values = ArrayUtil.expand(values, q, 1);
 		}
 		values[q] = value;
+		assert Bits.count(bits) == values.length;
 	}
 
 	public void remove(int index) {
 		if (Bits.test(bits, index)) {
 			Bits.clear(bits, index);
-			ArrayUtil.remove(values, Bits.count_before(bits, index), 1);
+			values = ArrayUtil.remove(values, Bits.count_before(bits, index), 1);
 		}
 	}
 
